25bcc85d894a29087db1b337b4c0241a9cc18bf7
gergo sipos <prosipinho@gmail.com>
2018-02-03 12:22:40 +0100

    Add project files.

---
+++ SSBO5G__Szakdolgozat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SSBO5G__Szakdolgozat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
